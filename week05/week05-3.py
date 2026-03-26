b142fd3a2823771f22c5cc686f0737e306c1dfdf
Terry623-dev <13750456@me.mcu.edu.tw>
2026-03-26 16:23:33 +0800

    week05

---
+++ week05/week05-3.py
@@ -0,0 +1,11 @@
+#week05-3.py
+#LeetCode 1207
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        counter = Counter(arr)
+        s = set()
+        for c in counter:
+            if counter[c] in s:
+                return False
+            s.add( counter[c] )
+        return True
