8c8c996dff938495a4ce79374c27ac64b4decea3
Terry623-dev <13750456@me.mcu.edu.tw>
2026-04-16 15:56:45 +0800

    week08

---
+++ week08/week08-5.py
@@ -0,0 +1,21 @@
+#week08-5.py
+#LeetCode 704
+from typing import List
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        N = len(nums)
+
+        if N == 1:
+            return 0
+
+        for i in range(N):
+            if i == 0:
+                if nums[i] > nums[i+1]:
+                    return i
+            elif i == N - 1:
+                if nums[i] > nums[i-1]:
+                    return i
+            else:
+                if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+                    return i
